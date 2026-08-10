2dc487b6cfd42bc18fee662f239401267bdc228f
Integra <integra@Mac-mini-Integra.local>
2026-08-10 15:09:43 +0200

    initial iOS project

---
+++ tps-mobile/tps_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  tps_mobileApp.swift
+//  tps-mobile
+//
+//  Created by Integra on 10/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct tps_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
